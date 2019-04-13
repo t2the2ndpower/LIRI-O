e7e1e7ac07e28997270ae2e024735db8a48418bd
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-04-13 15:13:51 -0400

    Edtied the readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # LIRI-O
-Node media search bot
+### A Stunning Node Media Search Bot
+Have you ever just wanted to search for a movie, song, or concert from the command line?
+Well, now is your chance.
+
+####Welcome to Liri-O!
+A sparkling command line search bot!
+
+##How to Use
+1. Open a command line
+2. Navigate to your **Liri-O** folder
+3. Type "Node liri.js " + one of the following commands
+    1. movie-this
+    2. concert-this
+    3. spotify-this-song
+4. and then type in your search item of choice (with a preceeding space of course, this is afterall run using process.argv)
+5. **Voila** hit 'enter' and your results mystically appear!
+
+##Enjoy!
+
--- liri.js
+++ liri.js
@@ -169,6 +169,8 @@ function pick(action, context) {
                 // If the axios was successful...
                 // Then log the body from the site!
 
+                
+
                 console.log("  ");
                 console.log("~~~~~~~~~~~~~~~~~----@@^&^@@----~~~~~~~~~~~~~~~~~~~~");
                 console.log("  ");
@@ -254,48 +256,11 @@ function pick(action, context) {
     }
 }
 
-// ### What Each Command Should Do
-// 1. `node liri.js concert-this <artist/band name here>`
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
 
-// 2. `node liri.js spotify-this-song '<song name here>'`
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-//    * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-//    * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-//    * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-//    * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-//    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
 
-// 3. `node liri.js movie-this '<movie name here>'`
-//   
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-//      * It's on Netflix!
-//    * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
 
-// 4. `node liri.js do-what-it-says`
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
-// ### BONUS
-// * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-// * Make sure you append each command you run to the `log.txt` file. 
-// * Do not overwrite your file each time you run a command.
 
-// ### Reminder: Submission on BCS
-// * Please submit the link to the Github Repository!
-// - - -
-
-// ### Minimum Requirements
-// Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed. Adding a README.md as well as adding this homework to your portfolio are required as well and more information can be found below.
-// - - -
 
 // ### Create a README.md
 // Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
@@ -312,7 +277,7 @@ function pick(action, context) {
 
 // This block of code will create a file called "log.txt".
 // Next, we store the text given to us from the command line.
-var text = process.argv;
+var text = process.argv + "  ~  ";
 
 // Next, we append the text into the "sample.txt" file.
 // If the file didn't exist, then it gets created on the fly.
